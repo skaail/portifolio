bdcffa18293135a75b5249f8b09ee3c9a4926823
Bruno <bruno.gianini@readmore.com.br>
2023-02-01 13:26:48 -0300

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
+    <Sidebar />
       <Routes>
         <Route index element={<Home />}/>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {HiMenuAlt3, HiHome, HiPaperClip, HiTerminal, HiPencilAlt, HiPhone} from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+
+function Sidebar() {
+
+    const menus = [
+        {name: 'Home', link:'/', icon: HiHome},
+        {name: 'Resumo', link:'/resumo', icon: HiPaperClip},
+        {name: 'Skills', link:'/skills', icon: HiTerminal},
+        {name: 'Projetos', link:'/projetos', icon: HiPencilAlt},
+        {name: 'Contato', link:'/contato', icon: HiPhone},
+    ]
+
+    const [open, setOpen] = useState(true);
+
+  return (
+    <section className='float-left'>
+        <div className={`bg-[#0e0e0e] min-h-screen ${open ? 'w-72':'w-16'} duration-500 text-gray-100 px-4`}>
+            <div className='py-3 flex justify-end'>
+                <HiMenuAlt3 size={26} className='cursor-pointer' onClick={()=>setOpen(!open)}/>
+            </div>
+            <div className='mt-4 flex flex-col gap-4 relative'>
+            { menus?.map((menu , i) =>                     
+                <Link to={menu?.link} key={i} className="flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md">
+                    <div>{React.createElement(menu?.icon, {size: '20'})}</div>
+                    <h2 className={`whitespace-pre duration-500 ${!open && 'opacity-0 translate-x-28 overflow-hidden'}`}>{menu?.name}</h2>
+                </Link>
+            )}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
