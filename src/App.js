f20f2a3d1c187df0e959583764220d6686a7f0c2
Bruno <bruno.gianini@readmore.com.br>
2023-02-01 12:21:56 -0300

    Rotas criadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
