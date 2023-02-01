8ba88fb9788c96650164a08c1e5d07a76cc009da
Bruno <bruno.gianini@readmore.com.br>
2023-02-01 16:26:26 -0300

    Linha do tempo completa

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,8 +17,8 @@ function Sidebar() {
     const [open, setOpen] = useState(false);
 
   return (
-    <section className='float-left'>
-        <div className={`bg-[#0e0e0e] min-h-screen ${open ? 'w-72':'w-16'} duration-500 text-gray-100 px-4`}>
+    <section className='float-left '>
+        <div className={`bg-[#0e0e0e] h-screen ${open ? 'w-72':'w-16'} duration-500 text-gray-100 px-4`}>
             <div className='py-3 flex justify-end'>
                 <HiMenuAlt3 size={26} className='cursor-pointer' onClick={()=>setOpen(!open)}/>
             </div>
--- src/pages/Educ.js
+++ src/pages/Educ.js
@@ -1,69 +1,103 @@
 import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import './educ.css'
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 import 'react-vertical-timeline-component/style.min.css';
-import { IoMdSchool } from "react-icons/io";
-import { MdWork } from "react-icons/md";
+import { TypeAnimation } from 'react-type-animation';
 
 function Educ() {
   return (
-    <div className='bg-gray-900 h-screen flex text-white items-center justify-center content-center'>
-        <VerticalTimeline>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                date="2017 - 2019"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<IoMdSchool />}
-            >
-                <h3 className="vertical-timeline-element-title">Ensino Médio Técnico</h3>
-                <h4 className="vertical-timeline-element-subtitle">FECAP</h4>
-                <p>Ensino Médio Técnico em Desenvolvimento de Sistemas</p>
+    <div className='bg-gray-900 h-screen flex-col flex text-white items-center justify-center content-center'>
+        <TypeAnimation 
+            sequence={[
+              'Linha do tempo'
+            ]}
+            wrapper="div"
+            cursor={true}
+            repeat={false}
+            style={{fontSize: '2em'}}
+        />
+        <div className='example overflow-auto py-28 w-4/5'>
+            <Timeline lineColor={'#ddd'}>
+                <TimelineItem
+                    key="001"
+                    dateText="02/2017 – 11/2019"
+                    style={{ color: '#e86971' }}
+                >
+                    <h3 className='text-white'>Ensino Médio Técnico</h3>
+                    <h4 className='text-white'>FECAP</h4>
+                    <p className='text-white'>
+                        Ensino Médio Técnico em Desenvolvimento de sistemas
+                    </p>
+                    <br></br>
+                    <p className='text-white'>
+                        Durante o curso fui capaz de realizar um trabalho de conclusão, 
+                        esse sendo um sistema de gerenciamento para um cliente real
+                    </p>
 
-            </VerticalTimelineElement>
+                </TimelineItem>
 
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                date="2020 - 2023"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<IoMdSchool />}
-            >
-                <h3 className="vertical-timeline-element-title">Bacharelado em Sistemas de Informação</h3>
-                <h4 className="vertical-timeline-element-subtitle">FIAP</h4>
 
-            </VerticalTimelineElement>
+                <TimelineItem
+                    key="002"
+                    dateText="02/2010 – 11/2023"
+                    style={{ color: '#e86971' }}
+                >
+                    <h3 className='text-white'>Bacharelado em Sistemas de Informação</h3>
+                    <h4 className='text-white'>FIAP</h4>
+                    <br></br>
+                    <p className='text-white'>
+                        Durante o curso fui capaz de realizar diversos trabalhos de iniciação cientifica, um deles, 
+                        totejuke um toten jukebox ultilizando tecnologia nfc para cadastro de musicas em cartões, premiado em 3° lugar na edição de 2022.
+                    </p>
+                    <br></br>
+                    <p className='text-white'>
+                        Realização de diversos cursos com certificados como Lidership Comunication e Gestão de Infraestrutura de TI
+                    </p>
 
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 243, 124)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 243, 124)' }}
-                date="2021 - 2022"
-                iconStyle={{ background: 'rgb(33, 243, 124)', color: '#fff' }}
-                icon={<MdWork />}
-            >
-                <h3 className="vertical-timeline-element-title">Estágio em Arquitetura de Sistemas</h3>
-                <h4 className="vertical-timeline-element-subtitle">DRIVEN.CX</h4>
-                <p>Estágio em Arquitetura para ecommerce feitos com VTex</p>
+                </TimelineItem>
 
-            </VerticalTimelineElement>
 
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 243, 124)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 243, 124)' }}
-                date="2022 - 2023"
-                iconStyle={{ background: 'rgb(33, 243, 124)', color: '#fff' }}
-                icon={<MdWork />}
-            >
-                <h3 className="vertical-timeline-element-title">Desenvolvedor Fullstack</h3>
-                <h4 className="vertical-timeline-element-subtitle">Readmore</h4>
-                <p>Desenvolvimento de Aplicativos Web Usando PHP, Laravel, React, Next.js, Angular, Node.js, VTex e Wordpress</p>
+                <TimelineItem
+                    key="003"
+                    dateText="06/2021 – 04/2022"
+                    style={{ color: '#e86971' }}
+                >
+                    <h3 className='text-white'>Estágio em Arquitetura de Solução</h3>
+                    <h4 className='text-white'>Driven.cx</h4>
+                    <br></br>
+                    <p className='text-white'>
+                        Durante meu estágio participei de vários projetos VTex como: 
+                    </p>
+                    <br></br>
+                    <ul className='text-white'>
+                        <li>- Tramontina (BR/LATAN / VTex)</li>
+                        <li>- Lacta (VTex)</li>
+                        <li>- Sicoob (VTex)</li>
+                    </ul>
+                </TimelineItem>
 
-            </VerticalTimelineElement>
 
-        </VerticalTimeline>
+                <TimelineItem
+                    key="004"
+                    dateText="04/2022 – 01/2023"
+                    style={{ color: '#e86971' }}
+                >
+                    <h3 className='text-white'>Desenvolvedor Fullstack</h3>
+                    <h4 className='text-white'>Readmore</h4>
+                    <br></br>
+                    <p className='text-white'>
+                        Enquanto estava trabalhando na Readmore fui pessoalmente resposável por vários projetos como:
+                    </p>
+                    <br></br>
+                    <ul className='text-white'>
+                        <li>- Corapost (Site de Marketing e Sistema de Gerenciamento / Wordpress e Laravel)</li>
+                        <li>- Paola da Vinci (Vtex)</li>
+                        <li>- Ponto Now (Angular)</li>
+                        <li>- Pokermob (Wordpress e Node.js)</li>
+                    </ul>
+                </TimelineItem>
+            </Timeline>
+        </div>
     </div>
   )
 }
